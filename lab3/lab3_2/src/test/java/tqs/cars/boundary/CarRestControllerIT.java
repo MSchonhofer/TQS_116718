04fef8bb711ee1f629cfadb73614ef77407fbdc8
MSchonhofer <101186731+MSchonhofer@users.noreply.github.com>
2023-03-12 23:12:43 +0000

    lab 3_2 finished

--- lab3/lab3_2/src/test/java/tqs/cars/boundary/CarRestControllerIT.java
+++ lab3/lab3_2/src/test/java/tqs/cars/boundary/CarRestControllerIT.java
@@ -1,16 +1,12 @@
 package tqs.cars.boundary;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testng.annotations.Test;
 import tqs.cars.GsCarsContainersApplication;
 import tqs.cars.JsonUtils;
 import tqs.cars.data.CarRepository;
@@ -49,7 +45,7 @@ public class CarRestControllerIT {
     }
 
     @Test
-    void givenEmployees_whenGetEmployees_thenStatus200() throws Exception {
+    void givenCars_whenGetCars_thenStatus200() throws Exception {
         Car car1 = new Car("Tesla", "X");
         car1.setCarId(1L);
         Car car2 = new Car("Porsche", "911");
@@ -64,7 +60,9 @@ public class CarRestControllerIT {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(2))))
                 .andExpect(jsonPath("$[0].maker", is("Tesla")))
-                .andExpect(jsonPath("$[1].model", is("X")));
+                .andExpect(jsonPath("$[0].model", is("X")))
+                .andExpect(jsonPath("$[1].maker", is("Porsche")))
+                .andExpect(jsonPath("$[1].model", is("911")));
     }
 
 }
---
+++ lab3/lab3_2/src/test/java/tqs/cars/boundary/CarRestControllerTemplateIT.java
@@ -0,0 +1,66 @@
+package tqs.cars.boundary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import tqs.cars.data.CarRepository;
+import tqs.cars.model.Car;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+public class CarRestControllerTemplateIT {
+    @LocalServerPort
+    int randomServerPort;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @AfterEach
+    public void resetDb() {
+        carRepository.deleteAll();
+    }
+
+    @Test
+    void whenValidInput_thenCreateCar() {
+        Car car1 = new Car("Tesla", "X");
+        ResponseEntity<Car> entity = restTemplate.postForEntity("/api/cars", car1, Car.class);
+
+        List<Car> found = carRepository.findAll();
+        assertThat(found).extracting(Car::getMaker).containsOnly("Tesla");
+    }
+
+    @Test
+    void givenCars_whenGetCars_thenStatus200()  {
+        Car car1 = new Car("Tesla", "X");
+        Car car2 = new Car("Porsche", "911");
+        carRepository.saveAndFlush(car1);
+        carRepository.saveAndFlush(car2);
+
+
+        ResponseEntity<List<Car>> response = restTemplate
+                .exchange("/api/cars", HttpMethod.GET, null, new ParameterizedTypeReference<List<Car>>() {
+                });
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).extracting(Car::getModel).containsExactly("X", "911");
+
+    }
+
+
+}
